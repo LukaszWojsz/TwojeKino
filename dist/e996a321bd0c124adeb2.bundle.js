6e689eb4c32de9af42cc19771749d563fedc6648
LukaszWojsz <lukasz.wojsz94@gmail.com>
2022-04-29 07:28:17 +0200

    edit

--- dist/e996a321bd0c124adeb2.bundle.js
+++ dist/e996a321bd0c124adeb2.bundle.js
@@ -279,7 +279,7 @@ function load() {
   localStorage.removeItem('selectedMovieIndex');
 
   if (username == null) {
-    window.location = 'TwojeKino';
+    window.location = 'TwojeKino/';
   }
 }
 
@@ -289,7 +289,7 @@ document.getElementById("logoutBtn").addEventListener('click', function () {
   console.log(loggedIn);
 
   if (loggedIn == false) {
-    window.location = 'TwojeKino';
+    window.location = 'TwojeKino/';
     localStorage.clear();
   }
 });
@@ -373,4 +373,4 @@ document.getElementById('encantoMovie').addEventListener('click', function () {
 
 /******/ })()
 ;
-//# sourceMappingURL=83003f19c6597bdebd08.bundle.js.map
+//# sourceMappingURL=e996a321bd0c124adeb2.bundle.js.map
